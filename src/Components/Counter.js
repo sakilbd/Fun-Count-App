13f3dabd3b1f0ce17de41f49a6ae8d270125e050
unknown <mdsakiluzzaman@gmail.com>
2022-02-28 21:57:14 +0600

    some progress

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Counter extends Component {
-    state = {  } 
+    state = {
+        count:0,
+        tags:['tag1','tag2','tag3']
+    } ;
+
+    styles={
+        
+        
+    };
+
     render() { 
         return (
-            <h1>Hello World </h1>
+            <div>
+                <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button variant="primary">Increment</button>
+                <ul>
+                    {this.state.tags.map(tag =><li key={tag}>{tag}</li>)}
+                </ul>
+            </div>
+            
         );
+    }
+    formatCount(){
+        const {count}=this.state;
+        return count === 0 ? <h1>Zero</h1> : count;
     }
 }
  
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Counter from './Components/Counter';
+import 'bootstrap/dist/css/bootstrap.css';
 
 
 
