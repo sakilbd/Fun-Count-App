f965e3470d0973a263da76f3a0adc76dd524934e
Sakil <mdsakiluzzaman@gmail.com>
2022-03-01 14:40:06 +0600

    delete buttons added and working properly

--- src/Components/Counters.js
+++ src/Components/Counters.js
@@ -6,17 +6,26 @@ class Counters extends Component {
     state = {
         counters:[
             { id: 1, value: 4 },
-            { id: 1, value: 0 },
-            { id: 1, value: 0 },
-            { id: 1, value: 0 },
+            { id: 2, value: 0 },
+            { id: 3, value: 0 },
+            { id: 4, value: 0 },
            
         ]
+    }
+     handleDelete = (counterId) =>{
+        const counters = this.state.counters.filter(c => c.id !==counterId);
+        this.setState({counters:counters});
     }
     render() {
         return (
             <div> 
                 {this.state.counters.map(counter => 
-                <Counter key={counter.id} value={counter.value} /> )}
+                <Counter 
+                key={counter.id}
+                value={counter.value} 
+                onDelete={this.handleDelete}
+                id={counter.id}/> )}
+
             </div>
            
         
