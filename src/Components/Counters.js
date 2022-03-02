c8fe84669822c37991c4e99c407f5755cce06644
Sakil <mdsakiluzzaman@gmail.com>
2022-03-02 14:49:09 +0600

    some progress

--- src/Components/Counters.js
+++ src/Components/Counters.js
@@ -16,10 +16,18 @@ class Counters extends Component {
         const counters = this.state.counters.filter(c => c.id !==counterId);
         this.setState({counters:counters});
     }
+    handleReset= () =>{
+        const counters = this.state.counters.map(c =>{
+            c.value = 0;
+            return c;
+        });
+        this.setState({counters});
+    }
+
     render() {
         return (
             <div> 
-                <button className="btn btn-primary btn-sm m-2">Reset</button>
+                <button onCLick={this.handleReset}className="btn btn-primary btn-sm m-2">Reset</button>
                 {this.state.counters.map(counter => 
                 <Counter 
                 key={counter.id}
